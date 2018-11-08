a62ba08837a0070a401cf2519c914497fb146a0d
math <gagm2354@usherbrooke.ca>
2018-11-08 11:55:03 -0500

    Bouton Fonctionne!!!!

--- Wall-e/src/main.cpp
+++ Wall-e/src/main.cpp
@@ -4,8 +4,16 @@
 void setup() {
   
   BoardInit();
+  pinMode(37, INPUT);
+  while(digitalRead(37)==0)
+  {
+    delay(10);
+    Serial.println(digitalRead(37));
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("starterrrr");
+  delay(1000);
 }
