91374c3831fe9ee365be799d7395fc7430981141
F-A <amyf2502@usherbrooke.ca>
2018-11-08 16:20:39 -0500

    Led fully fonctional

--- Wall-e/src/main.cpp
+++ Wall-e/src/main.cpp
@@ -1,19 +1,85 @@
 #include <Arduino.h>
 #include <LibRobus.h>
+#include <TimedAction.h>
+#include <QTRSensors.h>
+
+const int LED_V = 34;
+const int LED_J = 35;
+const int LED_R = 39;
+const int BOUTON_PIN = 37;
+const int M_GAUCHE = 0;
+const int M_DROITE = 0;
+
+
+void stop()
+{
+  digitalWrite(LED_R,HIGH);
+  MOTOR_SetSpeed(M_GAUCHE,0);
+  MOTOR_SetSpeed(M_DROITE,0);
+}
+void waitBouton()
+{
+  digitalWrite(LED_V,LOW);
+  digitalWrite(LED_R,HIGH);
+  while (digitalRead(BOUTON_PIN) == 0){}
+  while(digitalRead(BOUTON_PIN) == 1){} 
+  delay(50); // bouncing buffer
+  digitalWrite(LED_R,LOW);
+  digitalWrite(LED_V,HIGH);
+}
+
+void t_bouton()
+{
+  if (digitalRead(BOUTON_PIN) == 1)
+  {
+    while(digitalRead(BOUTON_PIN) == 1){}     // attend le relachement
+    stop();
+    delay(50); // bouncing buffer                             
+    waitBouton();                             // attend pour repartir
+  }
+  digitalWrite(LED_R,LOW);
+}
+
+void t_Couleur()
+{
+  //pet pet
+}
+
+void calibration()
+{
+  digitalWrite(LED_V,LOW);
+  for (int i =0; i<10;i++)
+  {
+    if(i%2 == 0)
+    {
+      digitalWrite(LED_J,HIGH);
+    }
+    else
+    digitalWrite(LED_J,LOW);
+    delay(500);
+  }
+
+} 
+TimedAction threadBouton = TimedAction(50,t_bouton);
+TimedAction threadCouleur = TimedAction(500,t_Couleur);
 
 void setup() {
   
   BoardInit();
-  pinMode(37, INPUT);
-  while(digitalRead(37)==0)
-  {
-    delay(10);
-    Serial.println(digitalRead(37));
-  }
+  pinMode(LED_V,OUTPUT);
+  digitalWrite(LED_V,LOW);
+  pinMode(LED_J,OUTPUT);
+  digitalWrite(LED_J,LOW);
+  pinMode(LED_R,OUTPUT);
+  digitalWrite(LED_R,HIGH);
+  pinMode(BOUTON_PIN, LOW);
+
+  waitBouton();
+  calibration();
+  waitBouton();
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("starterrrr");
-  delay(1000);
+  threadBouton.check();
 }
